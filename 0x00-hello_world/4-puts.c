6c3a47979e719a469d85df9579fd7ab09b464486
Frankchristianukah <ukah282@gmail.com>
2023-08-11 08:42:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ * Description - Printing lines
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");   
-       	return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
