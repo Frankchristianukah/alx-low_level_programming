e70861fd4d6c3c36db9d2ae975ae108387cbad14
Frankchristianukah <ukah282@gmail.com>
2023-09-03 13:08:18 -0700

    create_static_lib.sh

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,9 @@
+
 #include "main.h"
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best Software Eng\"\n\t- Frank Christian Ukah");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Frank Christian Ukah");
     return (0);
 }
 
