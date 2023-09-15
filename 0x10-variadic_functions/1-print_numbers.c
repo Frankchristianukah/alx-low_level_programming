a1d14e4dad1394bd31bd74c40d712878c90c7c31
Frankchristianukah <ukah282@gmail.com>
2023-09-15 06:22:17 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,30 +1,28 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-include <stdio.h>
+#include <stdio.h>
 /**
- * prtint_numbers - function that prints numbers.
+ * print_numbers - function that prints numbers.
  * @n: resepresents the number of arguments
- * @srparator: resepersents the seperator b/w the numbers
+ * @separator: resepresents the seperator b/w the numbers
  * Return: Always 0.
  */
- void print_numbers(const char *sepeartor, const unsing unt n. ...)
- {
-      /* creating va_list to store the variable argument list */
-     v_list my_nus;
-    unsiged int count;
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	/* creating va_list to store the variable argument list */
+	va_list my_nums;
+	unsigned int count;
 
-   /* init valist for the number of arguments */
-   va_start(my_nums, n);
+	/* init valist for the number of arguments */
+	va_start(my_nums, n);
 
-  /* loop through/access all argument stored in the valist */
-  for (count = 0; count <n; count++)
-  {
-          printf("%", va_arg(my_nums, unsigned int));
-      	 if (count < (n-1&& separator !+ NULL)
+	/* loop through/access all arguments stored in the valist */
+	for (count = 0; count < n; count++)
+	{
+		printf("%d", va_arg(my_nums, unsigned int));
+		if (count < (n - 1) && separator != NULL)
 			printf("%s", separator);
-			
 	}
 	va_end(my_nums);
-	printf("/n");
-
-	}	
+	printf("\n");
+}	
